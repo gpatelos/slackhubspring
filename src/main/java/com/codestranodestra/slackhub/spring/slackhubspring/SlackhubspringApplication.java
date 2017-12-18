4f432a16effe8c6b4b3aad28feefbdd31d96a5d3
gpatelos <greg.patselas@gmail.com>
2017-12-18 11:47:06 -0500

    first commit

---
+++ src/main/java/com/codestranodestra/slackhub/spring/slackhubspring/SlackhubspringApplication.java
@@ -0,0 +1,12 @@
+package com.codestranodestra.slackhub.spring.slackhubspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackhubspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackhubspringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codestranodestra/slackhub/spring/slackhubspring/SlackhubspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codestranodestra.slackhub.spring.slackhubspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlackhubspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
